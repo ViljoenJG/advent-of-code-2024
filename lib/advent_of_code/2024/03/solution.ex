0fce0819b46b8228ad062cc964b2a152cd9f8f14
Kobus Viljoen <ViljoenJG@users.noreply.github.com>
2024-12-03 16:52:13 +0100

    Day 3 solution

--- lib/advent_of_code/2024/03/solution.ex
+++ lib/advent_of_code/2024/03/solution.ex
@@ -14,40 +14,13 @@ defmodule AdventOfCode.Year2024.Day3.Solution do
   end
 
   defp solve_part1(input) do
-    Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)/, input)
+    Regex.scan(~r/mul\((\d+),(\d+)\)/, input)
     |> Enum.map(fn [_, a, b] -> String.to_integer(a) * String.to_integer(b) end)
     |> Enum.sum()
   end
 
   defp solve_part2(input) do
-    Regex.scan(~r/(mul)\((\d{1,3}),(\d{1,3})\)|don't|do(?!n't)/, input)
-    |> Enum.map(fn x ->
-      case x do
-        [_, "mul", a, b] -> String.to_integer(a) * String.to_integer(b)
-        ["don't"] -> :off
-        ["do"] -> :on
-      end
-    end)
-    |> then(&process(&1, true, 0))
+    Regex.replace(~r/(*CR)don't\(\).*?do\(\)/, input, "")
+    |> solve_part1
   end
-
-  defp process(stack, running, acc)
-
-  defp process([:off | tail], _, acc) do
-    process(tail, false, acc)
-  end
-
-  defp process([:on | tail], _, acc) do
-    process(tail, true, acc)
-  end
-
-  defp process([value | tail], true, acc) do
-    process(tail, true, acc + value)
-  end
-
-  defp process([_value | tail], false, acc) do
-    process(tail, false, acc)
-  end
-
-  defp process([], _, acc), do: acc
 end
--- test/2024/03/solution_test.exs
+++ test/2024/03/solution_test.exs
@@ -15,5 +15,14 @@ defmodule AdventOfCode.Year2024.Day3.SolutionTest do
       input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
       assert Day3.part2(input) == 48
     end
+
+    test "case2" do
+      input = """
+      xmul(2,4)&mul[3,7]!^don't()_mul(5,5)
+      +mul(32,64](mul(11,8)undo()?mul(8,5))
+      """
+
+      assert Day3.part2(input) == 48
+    end
   end
 end

dc2afe95b146c55d47c655e510aa8ce572cd6da0
Kobus Viljoen <ViljoenJG@users.noreply.github.com>
2024-12-03 08:31:51 +0100

    Day 3 solution

---
+++ lib/advent_of_code/2024/03/solution.ex
@@ -0,0 +1,53 @@
+defmodule AdventOfCode.Year2024.Day3.Solution do
+  @moduledoc """
+  Solution for day 3
+  """
+
+  def part1(input) do
+    input
+    |> solve_part1()
+  end
+
+  def part2(input) do
+    input
+    |> solve_part2()
+  end
+
+  defp solve_part1(input) do
+    Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)/, input)
+    |> Enum.map(fn [_, a, b] -> String.to_integer(a) * String.to_integer(b) end)
+    |> Enum.sum()
+  end
+
+  defp solve_part2(input) do
+    Regex.scan(~r/(mul)\((\d{1,3}),(\d{1,3})\)|don't|do(?!n't)/, input)
+    |> Enum.map(fn x ->
+      case x do
+        [_, "mul", a, b] -> String.to_integer(a) * String.to_integer(b)
+        ["don't"] -> :off
+        ["do"] -> :on
+      end
+    end)
+    |> then(&process(&1, true, 0))
+  end
+
+  defp process(value, running, acc)
+
+  defp process([:off | tail], _, acc) do
+    process(tail, false, acc)
+  end
+
+  defp process([:on | tail], _, acc) do
+    process(tail, true, acc)
+  end
+
+  defp process([value | tail], true, acc) do
+    process(tail, true, acc + value)
+  end
+
+  defp process([_value | tail], false, acc) do
+    process(tail, false, acc)
+  end
+
+  defp process([], _, acc), do: acc
+end
---
+++ test/2024/03/solution_test.exs
@@ -0,0 +1,19 @@
+defmodule AdventOfCode.Year2024.Day3.SolutionTest do
+  use ExUnit.Case, async: true
+
+  alias AdventOfCode.Year2024.Day3.Solution, as: Day3
+
+  describe "part1/1" do
+    test "case1" do
+      input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+      assert Day3.part1(input) == 161
+    end
+  end
+
+  describe "part2/1" do
+    test "case1" do
+      input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+      assert Day3.part2(input) == 48
+    end
+  end
+end
